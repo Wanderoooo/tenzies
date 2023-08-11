a60769e01e355f31994a22867c98ff5c7a4563dd
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-08-10 21:29:06 -0700

    added winning confetti

--- src/components/TenziesBoard.js
+++ src/components/TenziesBoard.js
@@ -1,12 +1,22 @@
 import NumBar from "./NumBar"
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 
 export default function TenziesBoard(props) {
 
+  const { width, height } = useWindowSize()
+
   return (
     <div className="flex justify-start items-center flex-col">
       <section>{props.isWin ? 
-      <h1 className="text-3xl font-bold m-8">You won!</h1> :
+      <section>
+      <h1 className="text-3xl font-bold m-8">You won!</h1>
+    <Confetti
+      width={width}
+      height={height}
+    />
+      </section> :
       <section>
       <h1 className="text-3xl font-bold">Tenzies</h1>
       <p className="max-w-xs just5 m-2">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
